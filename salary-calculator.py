d8e89fa7230eca2daa24645365597f221f56fdc1
Lindolfo Moizinho <lindolfomoizinho@gmail.com>
2024-11-05 19:38:17 -0300

    update

--- salary-calculator.py
+++ salary-calculator.py
@@ -6,9 +6,13 @@ base_salary = float(input("Enter the employee's base salary: "))
 
 total_sales = float(input("Enter the total sales amount: "))
 
-commission_percentage = float(input("Enter the commission percentage: "))
-
+commission_percentage = float(input("Enter the commission percentage %: "))
 
 total_salary = calculate_total_salary(base_salary, total_sales, commission_percentage)
 
-print(f"The total salary of the employee is: ${total_salary:.2f}")
+print(f"The total salary of the employee is: R${total_salary:.2f}")
+
+
+
+ git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
